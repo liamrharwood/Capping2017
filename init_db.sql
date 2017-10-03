347be0bf1182144100f834dc01aad06dfd8a04cd
liamrharwood <liamrharwood@gmail.com>
2017-10-03 15:44:57 -0400

    Add remaining tables to sql

--- init_db.sql
+++ init_db.sql
@@ -38,7 +38,18 @@ CREATE TABLE Users (
  PRIMARY KEY(user_id)
 );
 
- 
+ --
+ -- Follows Table
+ --
+
+DROP TABLE IF EXISTS Follows;
+
+CREATE TABLE Follows (
+	follower_id INTEGER NOT NULL REFERENCES Users(user_id),
+	followee_id INTEGER NOT NULL REFERENCES Users(user_id),
+ PRIMARY KEY(follower_id, followee_id)
+);
+
  --
  -- Communities Table
  --
@@ -59,10 +70,25 @@ CREATE TABLE Communities (
  -- Members Table
  --
 
+DROP TABLE IF EXISTS Members;
+
+CREATE TABLE Members (
+	user_id      INTEGER NOT NULL REFERENCES Users(user_id),
+	community_id INTEGER NOT NULL REFERENCES Communities(community_id),
+ PRIMARY KEY (user_id, community_id)
+);
+
  --
  -- Moderators Table
  --
 
+DROP TABLE IF EXISTS Moderators;
+
+CREATE TABLE Moderators (
+	user_id      INTEGER NOT NULL REFERENCES Users(user_id),
+	community_id INTEGER NOT NULL REFERENCES Communities(community_id),
+ PRIMARY KEY (user_id, community_id)
+);
 
  --
  -- Posts Table
@@ -72,7 +98,7 @@ DROP TABLE IF EXISTS Posts;
 
 CREATE TABLE Posts (
 	post_id         SERIAL,
-	user_id         INTEGER   REFERENCES Users(user_id),
+	user_id         INTEGER   NOT NULL REFERENCES Users(user_id),
 	upvotes         INTEGER   NOT NULL DEFAULT 0,
 	downvotes       INTEGER   NOT NULL DEFAULT 0,
 	body_text       TEXT,
@@ -86,3 +112,73 @@ CREATE TABLE Posts (
  --
  -- PostsToCommunities Table
  --
+
+DROP TABLE IF EXISTS PostsToCommunities;
+
+CREATE TABLE PostsToCommunities (
+	post_id      INTEGER NOT NULL REFERENCES Posts(post_id),
+	community_id INTEGER NOT NULL REFERENCES Communities(community_id),
+ PRIMARY KEY(post_id, community_id)
+);
+
+ --
+ -- Comments Table
+ --
+
+DROP TABLE IF EXISTS Comments;
+
+CREATE TABLE Comments (
+	comment_id SERIAL,
+	user_id    INTEGER NOT NULL REFERENCES Users(user_id),
+	post_id    INTEGER NOT NULL REFERENCES Posts(post_id),
+ PRIMARY KEY(comment_id)
+);
+
+ --
+ -- Reports Table
+ --
+
+DROP TABLE IF EXISTS Reports;
+
+CREATE TABLE Reports (
+	report_id     SERIAL,
+	post_id       INTEGER NOT NULL REFERENCES Posts(post_id),
+	report_reason TEXT    NOT NULL,
+ PRIMARY KEY(report_id)
+);
+
+ --
+ -- PostUpdates Table
+ --
+
+DROP TABLE IF EXISTS PostUpdates;
+
+CREATE TABLE PostUpdates (
+	post_update_id SERIAL,
+	post_id        INTEGER NOT NULL REFERENCES Posts(post_id),
+	body_text      TEXT    NOT NULL,
+ PRIMARY KEY (post_update_id)
+);
+
+ --
+ -- Hashtags Table
+ --
+
+DROP TABLE IF EXISTS Hashtags;
+
+CREATE TABLE Hashtags (
+	hashtag_name TEXT NOT NULL,
+ PRIMARY KEY(hashtag_name)
+);
+
+ --
+ -- TaggedPosts Table
+ --
+
+DROP TABLE IF EXISTS TaggedPosts;
+
+CREATE TABLE TaggedPosts (
+	hashtag_name TEXT    NOT NULL REFERENCES Hashtags(hashtag_name),
+	post_id      INTEGER NOT NULL REFERENCES Posts(post_id),
+ PRIMARY KEY(hashtag_name, post_id)
+);
