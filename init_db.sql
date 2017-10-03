43799f7c983416a70ff9a472c7f519492ef75cee
liamrharwood <liamrharwood@gmail.com>
2017-10-03 14:53:31 -0400

    Add Communities and Posts tables to sql

--- init_db.sql
+++ init_db.sql
@@ -31,21 +31,57 @@ CREATE TABLE Users (
  	profile_image_path TEXT,
  	bio                TEXT,
  	reputation_points  INTEGER   DEFAULT 0,
- 	is_administrator   BOOLEAN   NOT NULL,
- 	ban_status         BANSTATUS DEFAULT 'NOT_BANNED',
- 	create_date        TIMESTAMP NOT NULL,
+ 	is_administrator   BOOLEAN   NOT NULL DEFAULT FALSE,
+ 	ban_status         BANSTATUS NOT NULL DEFAULT 'NOT_BANNED',
+ 	create_date        TIMESTAMP NOT NULL DEFAULT now(),
  	update_date        TIMESTAMP,
  PRIMARY KEY(user_id)
- 	);
+);
 
  
  --
  -- Communities Table
  --
 
+DROP TABLE IF EXISTS Communities;
+
+CREATE TABLE Communities (
+	community_id SERIAL,
+	name         TEXT      NOT NULL,
+	description  TEXT      NOT NULL,
+	is_verified  BOOLEAN   DEFAULT FALSE,
+	create_date  TIMESTAMP NOT NULL DEFAULT now(),
+	ban_status   BANSTATUS DEFAULT 'NOT_BANNED',
+ PRIMARY KEY(community_id)
+);
+
+ --
+ -- Members Table
+ --
+
+ --
+ -- Moderators Table
+ --
+
+
  --
  -- Posts Table
  --
+
+DROP TABLE IF EXISTS Posts;
+
+CREATE TABLE Posts (
+	post_id         SERIAL,
+	user_id         INTEGER   REFERENCES Users(user_id),
+	upvotes         INTEGER   NOT NULL DEFAULT 0,
+	downvotes       INTEGER   NOT NULL DEFAULT 0,
+	body_text       TEXT,
+	post_title      TEXT      NOT NULL,
+	post_image_path TEXT,
+	create_date     TIMESTAMP NOT NULL,
+	is_complete     BOOLEAN   NOT NULL DEFAULT FALSE,
+ PRIMARY KEY(post_id)
+);
 
  --
  -- PostsToCommunities Table
