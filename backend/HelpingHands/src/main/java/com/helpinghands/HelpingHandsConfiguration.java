08ea5a7f07243a6b922efa578b4fcb231c2ed7f9
Liam Harwood <liamrharwood@gmail.com>
2017-10-10 14:11:31 -0400

    Initial commit for back end src

---
+++ backend/HelpingHands/src/main/java/com/helpinghands/HelpingHandsConfiguration.java
@@ -0,0 +1,26 @@
+package com.helpinghands;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.dropwizard.Configuration;
+import io.dropwizard.db.DataSourceFactory;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.xml.crypto.Data;
+
+public class HelpingHandsConfiguration extends Configuration {
+    @Valid
+    @NotNull
+    private DataSourceFactory database = new DataSourceFactory();
+
+    @JsonProperty("database")
+    public void setDataSourceFactory(DataSourceFactory factory) {
+        this.database = factory;
+    }
+
+    @JsonProperty("database")
+    public DataSourceFactory getDataSourceFactory() {
+        return database;
+    }
+
+}
---
+++ backend/HelpingHands/src/main/java/com/helpinghands/resources/UserResource.java
@@ -0,0 +1,26 @@
+package com.helpinghands.resources;
+
+import com.helpinghands.core.User;
+import com.helpinghands.dao.UserDAO;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("/users")
+@Produces(MediaType.APPLICATION_JSON)
+public class UserResource {
+    UserDAO userDAO;
+
+    public UserResource(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
+    @GET
+    public List<User> getAll() {
+        return userDAO.getAllUsers();
+    }
+
+}
