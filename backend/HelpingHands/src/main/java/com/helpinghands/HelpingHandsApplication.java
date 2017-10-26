50f4578a251adba5486daedc19450ef0f49c509e
Liam Harwood <liamrharwood@gmail.com>
2017-10-25 20:40:54 -0400

    Enable CORS

--- backend/HelpingHands/src/main/java/com/helpinghands/HelpingHandsApplication.java
+++ backend/HelpingHands/src/main/java/com/helpinghands/HelpingHandsApplication.java
@@ -16,10 +16,14 @@ import io.dropwizard.jdbi.DBIFactory;
 import io.dropwizard.migrations.MigrationsBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
 import org.skife.jdbi.v2.DBI;
 
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
 import java.security.Principal;
+import java.util.EnumSet;
 
 public class HelpingHandsApplication extends Application<HelpingHandsConfiguration> {
     public static void main(String[] args) throws Exception {
@@ -38,6 +42,19 @@ public class HelpingHandsApplication extends Application<HelpingHandsConfigurati
 
     @Override
     public void run(HelpingHandsConfiguration configuration, Environment environment) {
+        // Enable CORS headers
+        final FilterRegistration.Dynamic cors =
+                environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+        // Configure CORS parameters
+        cors.setInitParameter("allowedOrigins", "*");
+        cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
+        cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+
+        // Add URL mapping
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+
+
         final DBIFactory factory = new DBIFactory();
         final DBI jdbi = factory.build(environment, configuration.getDataSourceFactory(), "postgresql");
         final UserDAO userDAO = jdbi.onDemand(UserDAO.class);
