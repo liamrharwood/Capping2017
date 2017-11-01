24e482d97ebcb25d27f7fc484f5350c07c714b94
Liam Harwood <liamrharwood@gmail.com>
2017-11-01 16:05:46 -0400

    Allow all headers

--- backend/HelpingHands/src/main/java/com/helpinghands/HelpingHandsApplication.java
+++ backend/HelpingHands/src/main/java/com/helpinghands/HelpingHandsApplication.java
@@ -48,7 +48,7 @@ public class HelpingHandsApplication extends Application<HelpingHandsConfigurati
 
         // Configure CORS parameters
         cors.setInitParameter("allowedOrigins", "*");
-        cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
+        cors.setInitParameter("allowedHeaders", "*");
         cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
 
         // Add URL mapping
