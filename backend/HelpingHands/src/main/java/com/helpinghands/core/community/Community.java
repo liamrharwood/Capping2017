ce09798e5e5bc9e9329b45ae7c95a3c809f67ca8
liamrharwood <liamrharwood@gmail.com>
2017-11-06 12:36:31 -0500

    Add GET /communities to get communities followed by auth'd user, update docs, #9

---
+++ backend/HelpingHands/src/main/java/com/helpinghands/core/community/Community.java
@@ -0,0 +1,70 @@
+package com.helpinghands.core.community;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.helpinghands.core.BanStatus;
+
+import javax.validation.constraints.NotNull;
+import java.sql.Timestamp;
+
+public class Community {
+    @NotNull
+    @JsonProperty
+    private Integer communityId;
+
+    @NotNull
+    @JsonProperty
+    private String name;
+
+    @NotNull
+    @JsonProperty
+    private String description;
+
+    @NotNull
+    @JsonProperty
+    private boolean verified;
+
+    @NotNull
+    @JsonProperty
+    private Timestamp createDate;
+
+    @NotNull
+    @JsonProperty
+    private BanStatus banStatus;
+
+    public Community() {
+        // Jackson deserialization
+    }
+
+    public Community(Integer communityId, String name, String description, boolean verified, Timestamp createDate, BanStatus banStatus) {
+        this.communityId = communityId;
+        this.name = name;
+        this.description = description;
+        this.verified = verified;
+        this.createDate = createDate;
+        this.banStatus = banStatus;
+    }
+
+    public Integer getCommunityId() {
+        return communityId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isVerified() {
+        return verified;
+    }
+
+    public Timestamp getCreateDate() {
+        return createDate;
+    }
+
+    public BanStatus getBanStatus() {
+        return banStatus;
+    }
+}
--- backend/HelpingHands/src/main/java/com/helpinghands/resources/CommunityResource.java
+++ backend/HelpingHands/src/main/java/com/helpinghands/resources/CommunityResource.java
@@ -1,18 +1,32 @@
 package com.helpinghands.resources;
 
+import com.helpinghands.auth.UserPrincipal;
+import com.helpinghands.core.community.Community;
 import com.helpinghands.core.community.CommunityProfile;
 import com.helpinghands.dao.CommunityDAO;
+import com.helpinghands.dao.UserDAO;
+import io.dropwizard.auth.Auth;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Path("/communities")
 @Produces(MediaType.APPLICATION_JSON)
 public class CommunityResource {
     private CommunityDAO communityDAO;
+    private UserDAO userDAO;
 
-    public CommunityResource(CommunityDAO communityDAO) {
+    public CommunityResource(CommunityDAO communityDAO,
+                             UserDAO userDAO) {
         this.communityDAO = communityDAO;
+        this.userDAO = userDAO;
+    }
+
+    @GET
+    public List<Community> getCommunities(@Auth UserPrincipal userPrincipal) {
+        int userId = userDAO.getUserByUsername(userPrincipal.getName()).getId();
+        return communityDAO.getFollowedCommunities(userId);
     }
 
     @GET
